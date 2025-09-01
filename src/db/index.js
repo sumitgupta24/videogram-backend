5ce50eecbc26ebdc9ea01e06f8b946d16de079b2
Sumit Gupta <cricsumit24@gmail.com>
2025-09-01 23:42:34 +0530

    Util added

--- src/db/index.js
+++ src/db/index.js
@@ -8,8 +8,8 @@ const connectDB = async () => {
     }
     catch(error){
         console.log("MONGODB Connection FAILED:", error);
-         process.exit(1);
+        process.exit(1);
     }
 }
 
-export default connectDB
+export default connectDB  
